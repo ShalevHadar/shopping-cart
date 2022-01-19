c583ee1961c49473cd0b40d2dd29a585811545e3
Shalev Hadar <shdar94@gmail.com>
2022-01-19 16:01:11 +0200

    connection to db & boilerplate

---
+++ index.js
@@ -0,0 +1,25 @@
+// imports
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const db = require("./mysql/client");
+const port = process.env.PORT;
+const app = express();
+const userRoutes = require("./user/routes");
+
+// middleware
+app.use(express.json());
+app.use(cors());
+app.use(userRoutes);
+
+// connection to DB:
+
+db.connect((err) => {
+  if (!err) {
+    app.listen(port, () => {
+      console.log(`Starting server on port: ${port}`);
+    });
+  } else {
+    throw new Error("Can't connect to the db");
+  }
+});
